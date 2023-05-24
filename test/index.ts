27b68f37b76a8e641810a8faf8d2881b5a5985a8
nick thomas <nichoth@gmail.com>
2023-05-24 14:04:55 -0700

    test for decrypting

--- test/index.ts
+++ test/index.ts
@@ -1,9 +1,9 @@
 import { test } from 'tapzero'
 import { build, EdKeypair } from '@ucans/ucans'
 import { writeKeyToDid } from '@ssc-hermes/util'
-import { components } from '@ssc-hermes/node-components'
+import { components, createCryptoComponent } from '@ssc-hermes/node-components'
 import { Crypto } from '@oddjs/odd'
-import { aesEncrypt, aesDecrypt } from
+import { aesEncrypt, aesDecrypt, aesExportKey } from
     '@oddjs/odd/components/crypto/implementation/browser'
 import { fromString } from 'uint8arrays/from-string'
 import { toString } from 'uint8arrays/to-string'
@@ -31,13 +31,14 @@ test('create an identity', async t => {
     t.ok(identity.ucan, 'should incldue the UCAN')
 })
 
+let plainKey:CryptoKey
 test('can use the keys', async t => {
     // test that you can encrypt & decrypt with the symmetric key
     //   saved in identity
 
     // first decrypt the key
     const encryptedKey = identity.key[rootDid]
-    const decryptedKey = await decryptKey(encryptedKey, crypto)
+    const decryptedKey = plainKey = await decryptKey(encryptedKey, crypto)
 
     t.ok(decryptedKey instanceof CryptoKey, 'should return a CryptoKey')
 
@@ -51,4 +52,21 @@ test('can use the keys', async t => {
     )
 
     t.equal(decrypted, 'hello', 'should decrypt the original string')
+})
+
+// check that it is broken if you can't decrypt the key
+test('cannot decrypt the symmetric key with the wrong keys', async t => {
+    const crypto = await createCryptoComponent()
+
+    try {
+        const readableKey = await decryptKey(identity.key[rootDid], crypto)
+        const extracted = toString(await aesExportKey(readableKey), 'base64pad')
+        const otherExtracted = toString(await aesExportKey(plainKey), 'base64pad')
+        console.log('extracted', extracted)
+        console.log('other extracted', otherExtracted)
+        t.ok(!(extracted === otherExtracted),
+            'should not decrypt the key with a different crypto object')
+    } catch (err) {
+        t.ok(err, 'should throw an error because the crypto object is different')
+    }
 })
