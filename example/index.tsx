9149b98b966b0aec9d23f2d73789c84883134e6b
nick thomas <nichoth@gmail.com>
2023-10-05 23:58:35 -0700

    wip

--- example/index.tsx
+++ example/index.tsx
@@ -1,8 +1,17 @@
 import { render } from 'preact'
 import * as odd from '@oddjs/odd'
 import { writeKeyToDid } from '@ssc-hermes/util'
+import { useEffect } from 'preact/hooks'
+import { useSignal } from '@preact/signals'
 import PartySocket from 'partysocket'
-import { create } from '../src/index.js'
+import { customAlphabet } from '@nichoth/nanoid'
+import { numbers } from '@nichoth/nanoid-dictionary'
+// @ts-ignore  Don't know why it can't find this
+import { Button } from '@nichoth/components/button'
+import { Identity, create } from '../src/index.js'
+import '@nichoth/components/button.css'
+import '@nichoth/components/text-input.css'
+import './index.css'
 
 const program = await odd.program({
     namespace: { creator: 'test', name: 'testing' },
@@ -11,27 +20,6 @@ const program = await odd.program({
 
 const alicesCrytpo = program.components.crypto
 const rootDid = await writeKeyToDid(alicesCrytpo)
-const identity = await create(alicesCrytpo, {
-    humanName: 'alice',
-})
-
-// ------------------------------------------------------
-
-// connect to our server
-const partySocket = new PartySocket({
-    host: 'localhost:1999',
-    room: 'my-room',
-})
-
-// send a message to the server
-partySocket.send('Hello everyone')
-
-// print each incoming message from the server to console
-partySocket.addEventListener('message', (ev) => {
-    console.log(ev.data)
-})
-
-// ------------------------------------------------------
 
 render(<TheApp />, document.getElementById('root')!)
 
@@ -65,20 +53,152 @@ render(<TheApp />, document.getElementById('root')!)
  */
 
 function TheApp () {
+    const code = useSignal<string>('')
+    const id = useSignal<Identity|null>(null)
+    const status = useSignal<'add'|'join'|null>(null)
+    const isValidPin = useSignal<boolean>(false)
+
+    // @ts-ignore
+    window.id = id
+
+    console.log('rendering', code.value)
+
+    // * create an identity
+    useEffect(() => {
+        (async () => {
+            const identity = await create(alicesCrytpo, {
+                humanName: 'alice',
+            })
+
+            id.value = identity
+        })()
+    }, [])
+
+    // Connect to partykit
+    // listen for a message containing the new DID
+    function addDevice (ev) {
+        ev.preventDefault()
+        status.value = 'add'
+
+        const PIN = customAlphabet(numbers, 6)
+        code.value = ('' + PIN())
+
+        console.log('add a device...')
+
+        // connect to our server
+        const partySocket = new PartySocket({
+            host: 'localhost:1999',
+            room: code.value,
+            id: rootDid,
+            query: {
+                token: 'aaaaa',
+            },
+        })
+
+        partySocket.addEventListener('message', (ev) => {
+            console.log('from the server:', ev.data)
+
+            // add the device here...
+
+            partySocket.close()
+        })
+    }
+
+    /**
+     * Merge this with an existing Identity
+     */
+    async function join (ev:SubmitEvent) {
+        ev.preventDefault()
+        console.log('merge this device into another ID')
+        const el = (ev.target as HTMLFormElement).elements['pin']
+        const pin = el.value
+
+        // now try to connect via partykit
+        const partySocket = new PartySocket({
+            host: 'localhost:1999',
+            room: pin,
+            query: {
+                token: 'aaaaa'
+            }
+        })
+
+        partySocket.send(JSON.stringify({
+            newDid: await writeKeyToDid(alicesCrytpo)
+        }))
+
+        partySocket.close()
+    }
+
+    function pinInput (ev:InputEvent) {
+        const el = ev.target as HTMLInputElement
+        el.value = '' + el.value.slice(0, parseInt(el.getAttribute('maxlength')!))
+        const max = parseInt(el.getAttribute('maxlength')!)
+        const min = parseInt(el.getAttribute('minlength')!)
+        const valid = (el.value.length >= min && el.value.length <= max)
+        if (valid !== isValidPin.value) isValidPin.value = valid
+    }
+
+    function addToExisting (ev:SubmitEvent) {
+        ev.preventDefault()
+        status.value = 'join'
+    }
+
     return (<div>
+        {code.value ?
+            (<>
+                <strong>the PIN</strong>
+                <br />
+                <code>
+                    {code.value}
+                </code>
+
+                <hr />
+            </>) :
+            (status.value === 'join' ?
+                <form className="pin-form" onSubmit={join}>
+                    <div>
+                        <input name="pin" className="pin" type="number"
+                            minLength={6}
+                            maxLength={6}
+                            autoComplete="off"
+                            inputMode="numeric"
+                            id="pin-input"
+                            onInput={pinInput}
+                        />
+                    </div>
+
+                    <div>
+                        <Button type="submit"
+                            className="try-link"
+                            disabled={!isValidPin}
+                        >
+                            Link Devices
+                        </Button>
+                    </div>
+                </form> :
+                null)
+        }
+
+        <form onSubmit={addDevice}>
+            <Button type="submit">Add a device</Button>
+        </form>
+
+        <hr />
+
+        <form onSubmit={addToExisting}>
+            <Button type="submit">Add this device to an existing identity</Button>
+        </form>
+
+        <hr />
+
         <strong>The rootDid:</strong>
         <pre>
             <code>{rootDid}</code>
         </pre>
 
-        <strong>Devices:</strong>
-        <pre>
-            <code>{JSON.stringify(identity.devices, null, 2)}</code>
-        </pre>
-
         <strong>The identity:</strong>
         <pre>
-            <code>{JSON.stringify(identity, null, 2)}</code>
+            <code>{JSON.stringify(id.value, null, 2)}</code>
         </pre>
     </div>)
 }
