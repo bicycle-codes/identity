24f61a8fd2b3874b39595326f7a87f718e449ce8
nick thomas <nichoth@gmail.com>
2023-11-06 20:27:33 -0800

    better comments

--- example/server/index.ts
+++ example/server/index.ts
@@ -22,9 +22,9 @@ export default class WebSocketServer implements Party.Server {
 
     /**
      * @TODO implement this
-     *   - Would want to call a DB to check that the given DID is ok if
-     *     this is a new room. A new room means that this is a
-     *     request from an existing device.
+     *   - If this is a new room, Would want to call a DB to check that the
+     *     given DID is ok if this is a new room. A new room means that this is
+     *     a request from an existing device.
      *   - If this room already exists (if we already have an `existingDevice`),
      *     then the connection should be from a new device.
      */
@@ -47,8 +47,7 @@ export default class WebSocketServer implements Party.Server {
 
     onMessage (message:string, sender:Party.Connection) {
         // the only message we should get is the new DID
-        // need to tell the existing device the new DID, so they can sign
-        //   a UCAN authorizing
+        // need to tell the existing device the new DID
 
         if (!this.existingDevice) {
             // Should not happen.
@@ -61,68 +60,3 @@ export default class WebSocketServer implements Party.Server {
         )
     }
 }
-
-// see https://blog.partykit.io/posts/partykit-powers-realtime-avatars-in-epic-web
-
-// > All PartyKit server code that was needed to implement the real-time avatars
-// > feature to Kent’s course was the following:
-
-/*
-import type * as Party from 'partykit/server'
-
-type UserPayload = {
-    id: string
-    avatarUrl: string
-    name?: string | null | undefined
-}
-
-type Message =
-    | { type: 'remove-user'; payload: Pick<UserPayload, 'id'> }
-    | { type: 'add-user'; payload: UserPayload }
-    | { type: 'presence'; payload: { users: Array<UserPayload> } }
-
-export default class Server implements Party.Server {
-    options: Party.ServerOptions = { hibernate: true }
-    constructor(party: Party.Party) {
-        this.party = party
-    }
-
-    updateUsers() {
-        const presenceMessage = JSON.stringify(this.getPresenceMessage())
-        for (const connection of this.party.getConnections<UserPayload>()) {
-            connection.send(presenceMessage)
-        }
-    }
-
-    getPresenceMessage(): Message {
-        const users = new Map<string, UserPayload>()
-        for (const connection of this.party.getConnections<UserPayload>()) {
-            const user = connection.state
-            if (user) users.set(user.id, user)
-        }
-        return {
-            type: 'presence',
-            payload: { users: Array.from(users.values()) },
-        } satisfies Message
-    }
-
-    onMessage(message: string, sender: Party.Connection<UserPayload>) {
-        const user = JSON.parse(message) as Message
-        if (user.type === 'add-user') {
-            sender.setState(user.payload)
-            this.updateUsers()
-        } else if (user.type === 'remove-user') {
-            sender.setState(null)
-            this.updateUsers()
-        }
-    }
-
-    onClose() {
-        this.updateUsers()
-    }
-
-    onError() {
-        this.updateUsers()
-    }
-}
-*/
